7a110b98fb64668bd6f455ceeec2482261241f56
1001OKIT <okitondo.kayembe@strathmore.edu>
2026-06-23 23:57:27 +0300

    Implement contact form validation

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ document.getElementById("darkModeBtn").addEventListener("click", function () {
     document.body.classList.toggle("bg-dark");
     document.body.classList.toggle("text-white");
 });
+
+const form = document.getElementById("contactForm");
+
+if (form) {
+
+    form.addEventListener("submit", function (event) {
+
+        const name =
+            document.getElementById("name").value;
+
+        const email =
+            document.getElementById("email").value;
+
+        const message =
+            document.getElementById("message").value;
+
+        if (name === "" ||
+            email === "" ||
+            message === "") {
+
+            alert("Please fill in all fields.");
+
+            event.preventDefault();
+        }
+
+    });
+
+}
